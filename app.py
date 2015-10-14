66cf62bf347d1ed48af0f46faafbfe556f33fa96
LINDSAY <lf2508@columbia.edu>
2015-10-13 23:10:58 -0400

    Lfhmwk4
    lf2508, submission

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask
 from flask import render_template
+from flask import Response
+from Queue import Queue
 
 import json
 import time
 import sys
+import random
 
 import pyorient
 
 app = Flask(__name__)
+q = Queue()
+
+def event_stream():
+    while True:
+        result = q.get()
+        yield 'data: %s\n\n' % str(result)
+@app.route('/eventSource/')
+def sse_source():
+    return Response( event_stream(), mimetype='text/event-stream' )
 
 @app.route("/")
 def index():
@@ -15,6 +27,15 @@ def index():
 
 @app.route("/getData/")
 def getData():
+
+	q.put("starting data query...")
+
+	lat1 = str(request.args.get('lat1'))
+	lng1 = str(request.args.get('lng1'))
+	lat2 = str(request.args.get('lat2'))
+	lng2 = str(request.args.get('lng2'))
+
+	print "received coordinates: [" + lat1 + ", " + lat2 + "], [" + lng1 + ", " + lng2 + "]"
 	
 	client = pyorient.OrientDB("localhost", 2424)
 	session_id = client.connect("root", "password")
@@ -38,6 +59,8 @@ def getData():
 	query = 'SELECT FROM Listing WHERE latitude BETWEEN {} AND {} AND longitude BETWEEN {} AND {}'
 
 	records = client.command(query.format(lat1, lat2, lng1, lng2))
+	random.shuffle(records)
+	records = records[:100]
 
 	numListings = len(records)
 	print 'received ' + str(numListings) + ' records'
@@ -54,8 +77,9 @@ def getData():
 		feature["geometry"]["coordinates"] = [record.latitude, record.longitude]
 
 		output["features"].append(feature)
-
+	q.put('idle')
 	return json.dumps(output)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
